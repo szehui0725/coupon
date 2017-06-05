dc55133fb28bde48a0026b404abe7d4146ae9f8d
Sze Hui <szehui_teo0725@hotmail.com>
2017-06-05 11:21:38 +0800

    socialShare

--- www/js/function.js
+++ www/js/function.js
@@ -634,7 +634,36 @@ function paymentAction() {
     // window.molpay.transactionRequest(paymentDetails, molpayCallback);
     // window.molpay.closeMolpay();
   });
+  $$('#fbShare').on('click', function() {
+    window.plugins.socialsharing.shareViaFacebook('Message via Facebook',
+    null /* img */,
+    null /* url */,
+    null,
+    function(errormsg){alert("Error: Cannot Share")}
+  );
+});
 
+  $$('#twitterShare').on('click', function() {
+    window.plugins.socialsharing.shareViaTwitter('Message via Twitter',
+    null /* img */,
+    'http://twitter.com/',
+    null,
+    function(errormsg){alert("Error: Cannot Share")}
+  );
+  });
+
+  $$('#whatsappShare').on('click', function() {
+  window.plugins.socialsharing.shareViaWhatsApp('Message via WhatsApp',
+  null /* img */,
+  "http://pointdeveloper.com/" /* url */,
+  null,
+  function(errormsg) {alert("Error: Cannot share")}
+  );
+};
+
+  $$('#otherShare').on('click', function() {
+      window.plugins.socialsharing.share('General share Message', null, null, null);
+  });
 }
 
 function homeAction() {
