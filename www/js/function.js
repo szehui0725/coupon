3ef93c30329b838467c2065d1668b7e108f614c5
Sze Hui <szehui_teo0725@hotmail.com>
2017-06-06 16:23:06 +0800

    chart

--- www/js/function.js
+++ www/js/function.js
@@ -28,6 +28,9 @@ $$(document).on('pageInit', function(e) {
     case 'share':
       shareAction();
       break;
+    case 'chart':
+      chartAction();
+      break;
     case 'qrcode':
       qrcodeAction();
       break;
@@ -540,31 +543,30 @@ $$('.home-toolbar a').on('click', function(e, i) {
   $$(this).addClass('selected');
 });
 
+function chartAction() {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [{
+        label: "My First dataset",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+      }]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+}
+
 function qrcodeAction() {
   $$('#qrCode').on('click', function() {
-    // window.plugins.barcodeScanner.scan(
-    //      function (result) {
-    //          alert("We got a barcode\n" +
-    //                "Result: " + result.text + "\n" +
-    //                "Format: " + result.format + "\n" +
-    //                "Cancelled: " + result.cancelled);
-    //      },
-    //      function (error) {
-    //          alert("Scanning failed: " + error);
-    //      },
-    //      {
-    //          preferFrontCamera : true, // iOS and Android
-    //          showFlipCameraButton : true, // iOS and Android
-    //          showTorchButton : true, // iOS and Android
-    //          torchOn: true, // Android, launch with the torch switched on (if available)
-    //          prompt : "Place a barcode inside the scan area", // Android
-    //          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-    //          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-    //          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-    //          disableAnimations : true, // iOS
-    //          disableSuccessBeep: false // iOS
-    //      }
-    //   );
     cordova.plugins.barcodeScanner.scan(
       function(result) {
         if (!result.cancelled) {
@@ -592,26 +594,6 @@ function qrcodeAction() {
     );
   });
 }
-
-// cordova.plugins.barcodeScanner.scan(
-//    function (result) {
-//         if(!result.cancelled){
-//                // In this case we only want to process QR Codes
-//                if(result.format == "QR_CODE"){
-//                     var value = result.text;
-//                     // This is the retrieved content of the qr code
-//                     console.log(value);
-//                }else{
-//                   alert("Sorry, only qr codes this time ;)");
-//                }
-//         }else{
-//           alert("The user has dismissed the scan");
-//         }
-//      },
-//      function (error) {
-//           alert("An error ocurred: " + error);
-//      }
-// );
 
 function paymentAction() {
   $$('#buyNowBtn').on('click', function() {
