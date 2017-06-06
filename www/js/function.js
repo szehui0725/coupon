ac31447e5474aa06465bad2b06b567631c579557
Sze Hui <szehui_teo0725@hotmail.com>
2017-06-06 15:09:10 +0800

    QRcode

--- www/js/function.js
+++ www/js/function.js
@@ -566,24 +566,24 @@ function qrcodeAction() {
     //      }
     //   );
     cordova.plugins.barcodeScanner.scan(
-   function (result) {
-        if(!result.cancelled){
-               // In this case we only want to process QR Codes
-               if(result.format == "QR_CODE"){
-                    var value = result.text;
-                    // This is the retrieved content of the qr code
-                    console.log(value);
-               }else{
-                  alert("Sorry, only qr codes this time ;)");
-               }
-        }else{
+      function(result) {
+        if (!result.cancelled) {
+          // In this case we only want to process QR Codes
+          if (result.format == "QR_CODE") {
+            var value = result.text;
+            // This is the retrieved content of the qr code
+            alert(value);
+          } else {
+            alert("Sorry, only qr codes this time ;)");
+          }
+        } else {
           alert("The user has dismissed the scan");
         }
-     },
-     function (error) {
-          alert("An error ocurred: " + error);
-     }
-);
+      },
+      function(error) {
+        alert("An error ocurred: " + error);
+      }
+    );
   });
 }
 
