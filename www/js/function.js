62dd787e8712bde241d04391e449f84acaa338eb
Sze Hui <szehui_teo0725@hotmail.com>
2017-06-06 11:55:12 +0800

    qrcode

--- www/js/function.js
+++ www/js/function.js
@@ -543,16 +543,28 @@ $$('.home-toolbar a').on('click', function(e, i) {
 function qrcodeAction() {
   $$('#qrCode').on('click', function() {
     cordova.plugins.barcodeScanner.scan(
-      function(result) {
-        alert("A barcode has been scanned \n" +
-          "Result: " + result.text + "\n" +
-          "Format: " + result.format + "\n" +
-          "Cancelled: " + result.cancelled);
-      },
-      function(error) {
-        alert("Scanning failed: " + error);
-      }
-    );
+         function (result) {
+             alert("We got a barcode\n" +
+                   "Result: " + result.text + "\n" +
+                   "Format: " + result.format + "\n" +
+                   "Cancelled: " + result.cancelled);
+         },
+         function (error) {
+             alert("Scanning failed: " + error);
+         },
+         {
+             preferFrontCamera : true, // iOS and Android
+             showFlipCameraButton : true, // iOS and Android
+             showTorchButton : true, // iOS and Android
+             torchOn: true, // Android, launch with the torch switched on (if available)
+             prompt : "Place a barcode inside the scan area", // Android
+             resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+             formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+             orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+             disableAnimations : true, // iOS
+             disableSuccessBeep: false // iOS
+         }
+      );
   });
 }
 
